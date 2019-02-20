ab8daf8fe9b51ea7a483f59d22c42f25c5ea5c4a
vmkernel <Aleksey.I.Ivanov@gmail.com>
2019-02-20 18:23:45 +0300

    Implemented and tested multi-threaded compression with continuous source file reading and destination file writing

--- Program.cs
+++ Program.cs
@@ -73,33 +73,30 @@ namespace GZipTest
         // Threads pool
         static GZipThread[] _threads;
 
-        // Sequence number
-        static long _sequenceNumber;
+        // Read sequence number
+        static long _readSequenceNumber;
+
+        // Read sequence number
+        static long _writeSequenceNumber;
 
         // Threads count according to CPU cores count
         static int _threadCount;
 
-        // Data buffer which is shared between threads
-        static byte[][] _srcBuffer;
-
-        // Destination data buffer which is shared between threads
-        static byte[][] _dstBuffer;
-
-        // Pool of threads
-        //static Thread[] _threads;
-
         // HARDCODE: size of a buffer per thread
-        static int _chunkSize = 512 * 1024 * 1024; // 512M per thread
+        static int _chunkSize = 256 * 1024 * 1024; // 512M per thread
 
         // HARDCODE: name of source file to pack
-        static String _srcFileName = @"E:\Downloads\Movies\Mad.Max.Fury.Road.2015.1080p.BluRay.AC3.x264-ETRG.mkv";
-        //static String _srcFileName = @"D:\tmp\Iteration4-2x4CPU_16GB_RAM.blg";
+        //static String _srcFileName = @"E:\Downloads\Movies\Mad.Max.Fury.Road.2015.1080p.BluRay.AC3.x264-ETRG.mkv";
+        static String _srcFileName = @"D:\tmp\2016-02-03-raspbian-jessie.img";
 
         // Source file stream to read data from
-        static FileStream inputStream;
+        static FileStream _inputStream;
 
-        // Destination file stream to read data from
-        static FileStream _dstFileStream;
+        // Source file stream to write data to
+        static FileStream _outputStream;
+
+        // 
+        static bool _hasFileReadThreadExited;
 
         // Threaded compression function
         static void BlockCompressionThread(object parameter)
@@ -113,121 +110,31 @@ namespace GZipTest
                 }
                 _threads[threadIndex].OutputBuffer = outputStream.ToArray();
             }
-
-            /*
-            int threadIndex = (int)parameter;
-            using (MemoryStream outputStream = new MemoryStream(_srcBuffer[threadIndex].Length))
-            {
-                using (GZipStream compressionStream = new GZipStream(outputStream, CompressionMode.Compress))
-                {
-                    compressionStream.Write(_srcBuffer[threadIndex], 0, _srcBuffer[threadIndex].Length);
-                }
-                _dstBuffer[threadIndex] = outputStream.ToArray();
-            }
-            */
         }
 
         // Threaded file read function
         static void InputFileReadThread(object parameter)
         {
             String fileName = (String)parameter;
-            inputStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-
-            int bytesRead;
-            int bufferSize;
-
-            while (inputStream.Position < inputStream.Length)
+            using (_inputStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
             {
-                // TODO: Add 'thread slot free' event from running threads if no thread has been started during current loop
-                for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
+                int bytesRead;
+                int bufferSize;
+                Boolean hasStartedThread;
+
+                while (_inputStream.Position < _inputStream.Length)
                 {
-                    if (_threads[threadIndex] == null)
+                    // TODO: Add 'thread slot free' event from running threads if no thread has been started during current loop
+                    hasStartedThread = false;
+
+                    for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
                     {
-                        // read data
-                        if ((inputStream.Length - inputStream.Position) < _chunkSize)
+                        if (_threads[threadIndex] == null)
                         {
-                            bufferSize = (int)(inputStream.Length - inputStream.Position);
-                        }
-                        else
-                        {
-                            bufferSize = _chunkSize;
-                        }
-
-                        if (bufferSize <= 0)
-                        {
-                            break;
-                        }
-
-                        _threads[threadIndex] = new GZipThread();
-                        _threads[threadIndex].InputBuffer = new byte[bufferSize];
-
-                        bytesRead = inputStream.Read(_threads[threadIndex].InputBuffer, 0, _threads[threadIndex].InputBuffer.Length);
-                        if (bytesRead <= 0)
-                        {   
-                            break; // Reached the end of the file
-                        }
-
-                        _threads[threadIndex].WorkerThread = new Thread(BlockCompressionThread);
-                        _threads[threadIndex].SequenceNumber = _sequenceNumber;
-                        _sequenceNumber++;
-
-                        _threads[threadIndex].WorkerThread.Start(threadIndex);
-                        
-                    }
-                }
-            }
-        }
-
-        // Threaded file write function
-        static void OutputFileWriteThread(object parameter)
-        {/*
-            String fileName = (String)parameter;
-
-            _dstFileStream = new FileStream(fileName, FileMode.CreateNew);
-
-            while (true) // implement a kill-switch
-            {
-                for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
-                {
-                    if (_threads[threadIndex] != null && _threads[threadIndex].ThreadState == ThreadState.Stopped)
-                    {
-                        // check sequence number
-                        // write data
-                    }
-                }
-            }*/
-        }
-
-        static int Main(string[] args)
-        {
-            // Init local variables
-            _threadCount = Environment.ProcessorCount;
-            _threads = new GZipThread[_threadCount];
-            _sequenceNumber = 0;
-
-            FileInfo srcFileInfo = new FileInfo(_srcFileName);
-            String dstFileName = srcFileInfo.FullName + ".gz";
-
-            Thread inputFileReadThread = new Thread(InputFileReadThread);
-            inputFileReadThread.Start(_srcFileName);
-
-            /*
-            int bytesRead;
-            int bufferSize;
-            using (FileStream sourceStream = new FileStream(_srcFileName, FileMode.Open, FileAccess.Read))
-            {
-                using (FileStream destinationStream = new FileStream(dstFileName, FileMode.Create))
-                {
-                    while (sourceStream.Position < sourceStream.Length)
-                    {
-                        _srcBuffer = new byte[_threadCount][];
-                        _dstBuffer = new byte[_threadCount][];
-
-                        for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
-                        {
-                            if ((sourceStream.Length - sourceStream.Position) < _chunkSize)
+                            // read data
+                            if ((_inputStream.Length - _inputStream.Position) < _chunkSize)
                             {
-                                bufferSize = (int)(sourceStream.Length - sourceStream.Position);
+                                bufferSize = (int)(_inputStream.Length - _inputStream.Position);
                             }
                             else
                             {
@@ -239,42 +146,103 @@ namespace GZipTest
                                 break;
                             }
 
-                            _srcBuffer[threadIndex] = new byte[bufferSize];
-                            bytesRead = sourceStream.Read(_srcBuffer[threadIndex], 0, bufferSize);
+                            _threads[threadIndex] = new GZipThread();
+                            _threads[threadIndex].InputBuffer = new byte[bufferSize];
 
-                            _threads[threadIndex] = new Thread(BlockCompressionThread);
-                            _threads[threadIndex].Start(threadIndex);
-                        }
-
-                        // Waiting for all threads to exit
-                        Boolean bAllThreadsFinished;
-                        do
-                        {
-                            bAllThreadsFinished = true;
-                            for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
+                            bytesRead = _inputStream.Read(_threads[threadIndex].InputBuffer, 0, _threads[threadIndex].InputBuffer.Length);
+                            if (bytesRead <= 0)
                             {
-                                if (_threads[threadIndex] != null && _threads[threadIndex].ThreadState == ThreadState.Running)
-                                {
-                                    bAllThreadsFinished = false;
-                                }
+                                break; // Reached the end of the file
                             }
 
-                        } while (!bAllThreadsFinished);
+                            _threads[threadIndex].WorkerThread = new Thread(BlockCompressionThread);
+                            _threads[threadIndex].SequenceNumber = _readSequenceNumber;
+                            _threads[threadIndex].WorkerThread.Name = String.Format("Block compression (idx: {0}, seq: {1}", threadIndex, _readSequenceNumber);
+                            _readSequenceNumber++;
 
-                        // Writing results to the output file
-                        for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
+                            _threads[threadIndex].WorkerThread.Start(threadIndex);
+                            hasStartedThread = true;
+                        }
+                    }
+
+                    // Waiting for a thread to exit
+                    if (!hasStartedThread)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+
+                _hasFileReadThreadExited = true;
+            }
+        }
+
+        // Threaded file write function
+        static void OutputFileWriteThread(object parameter)
+        {
+            String fileName = (String)parameter;
+
+            using (_outputStream = new FileStream(fileName, FileMode.Create))
+            {
+                Boolean hasWrittenData;
+                Boolean hasNoThreads;
+                while (true) // implement a kill-switch
+                {
+                    hasWrittenData = false;
+                    hasNoThreads = true;
+
+                    for (int threadIndex = 0; threadIndex < _threadCount; threadIndex++)
+                    {
+                        if (_threads[threadIndex] != null)
                         {
-                            if ( _dstBuffer[threadIndex] != null && _dstBuffer[threadIndex].Length > 0)
+                            hasNoThreads = false;
+                        }
+
+                        if (_threads[threadIndex] != null &&
+                            _threads[threadIndex].WorkerThread != null &&
+                            _threads[threadIndex].WorkerThread.ThreadState == ThreadState.Stopped)
+                        {
+                            if (_threads[threadIndex].SequenceNumber == _writeSequenceNumber)
                             {
-                                destinationStream.Write(_dstBuffer[threadIndex], 0, _dstBuffer[threadIndex].Length);
+                                _outputStream.Write(_threads[threadIndex].OutputBuffer, 0, _threads[threadIndex].OutputBuffer.Length);
+                                _writeSequenceNumber++;
+                                _threads[threadIndex] = null;
+                                hasWrittenData = true;
                             }
                         }
-                    } // Advancing to the next block of data until the end of the source file
+                    }
 
-                } // using destinationStream
+                    if (!hasWrittenData)
+                    {
+                        Thread.Sleep(1000);
+                    }
 
-            } // using sourceStream
-            */
+                    if (_hasFileReadThreadExited && hasNoThreads)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            // Init local variables
+            _threadCount = Environment.ProcessorCount;
+            _threads = new GZipThread[_threadCount];
+            _readSequenceNumber = 0;
+            _writeSequenceNumber = 0;
+
+            FileInfo srcFileInfo = new FileInfo(_srcFileName);
+            String dstFileName = srcFileInfo.FullName + ".gz";
+
+            Thread inputFileReadThread = new Thread(InputFileReadThread);
+            inputFileReadThread.Name = "Read input file";
+            inputFileReadThread.Start(_srcFileName);
+
+            Thread outputFileWriteThread = new Thread(OutputFileWriteThread);
+            outputFileWriteThread.Name = "Write output file";
+            outputFileWriteThread.Start(dstFileName);
+
             Console.ReadLine();
 
             return 0;
