3262893752c61643618dc64894c5908cc59bb507
vmkernel <Aleksey.I.Ivanov@gmail.com>
2019-03-11 20:50:12 +0300

    Added: exceptions handling Edited: comments

--- CGZipBlock.cs
+++ CGZipBlock.cs
@@ -2,27 +2,34 @@
 
 namespace GZipTest
 {
+    // Represents a block of data compressed using GZipStream class 
+    // with the data itself, and metadata (uncompressed and compressed size) which is stored in an ouput file along with the data.
     public class CGZipBlock
     {
-        // Size (in bytes) of buffer which is used to store size of uncompressed data block
+        #region FIELDS
+        #region Metadata
+        // Buffer size for uncompressed data block size info
+        // Uses to allocate uncompressed data block size buffer and corresponding read operations for the buffer
         private static Int32 MetadataUncompressedBlockSizeBufferLength
         {
             get
             {
-                return sizeof(Int32); // TODO: replace with relative sizeof calculation
+                return sizeof(Int32);
             }
         }
 
-        // Size (in bytes) of buffer which is used to store size of compressed data block
+        // Buffer size for compressed data block size info
+        // Uses to allocate compressed data block size buffer and corresponding read operations for the buffer
         private static Int32 MetadataCompressedBlockSizeBufferLength
         {
             get
             {
-                return sizeof(Int32); // TODO: replace with relative sizeof calculation
+                return sizeof(Int32);
             }
         }
 
-        // Size (in bytes) of metadata
+        // Buffer size for all metadate
+        // Uses to allocate metadata buffer and corresponding read operations for the buffer
         public static Int32 MetadataSize
         {
             get
@@ -30,6 +37,7 @@ namespace GZipTest
                 return MetadataCompressedBlockSizeBufferLength + MetadataUncompressedBlockSizeBufferLength;
             }
         }
+        #endregion
 
         // Size (in bytes) of an original (uncompressed) block of data
         public Int32 DataSizeUncompressed { get; set; }
@@ -39,6 +47,7 @@ namespace GZipTest
         {
             get
             {
+                // Gets from allocated data buffer
                 return Data.Length;
             }
         }
@@ -47,6 +56,7 @@ namespace GZipTest
         private Byte[] s_data;
         public Byte[] Data
         {
+            // In order to prevent NullReferrenceException, gets and sets an empty array instead of null
             get
             {
                 if (s_data == null)
@@ -65,40 +75,66 @@ namespace GZipTest
                 s_data = value;
             }
         }
+        #endregion
 
-        // Converts metadata and compressed data to byte array (to be able to write it to an output file)
+        #region FUNCTIONS AND METHODS
+        // Converts compressed data and its metadata to byte array (which might be written to an output file, for example)
         public Byte[] ToByteArray()
         {
-            Byte[] uncompressedSizeBuffer = BitConverter.GetBytes(DataSizeUncompressed);
-            Byte[] compressedSizeBuffer = BitConverter.GetBytes(DataSizeCompressed);
+            try
+            {
+                // Converting uncompressed and compressed size from integer value to byte array
+                Byte[] uncompressedSizeBuffer = BitConverter.GetBytes(DataSizeUncompressed);
+                Byte[] compressedSizeBuffer = BitConverter.GetBytes(DataSizeCompressed);
 
-            Int32 resultantBufferLength = uncompressedSizeBuffer.Length + compressedSizeBuffer.Length + Data.Length;
-            Byte[] resultantBuffer = new Byte[resultantBufferLength];
+                if ((uncompressedSizeBuffer.Length + compressedSizeBuffer.Length) != CGZipBlock.MetadataSize)
+                {
+                    throw new Exception("Expected and calculated metadata block size mismatch.");
+                }
 
-            Array.Copy(uncompressedSizeBuffer, 0, resultantBuffer, 0, uncompressedSizeBuffer.Length);
-            Array.Copy(compressedSizeBuffer, 0, resultantBuffer, uncompressedSizeBuffer.Length, compressedSizeBuffer.Length);
-            Array.Copy(Data, 0, resultantBuffer, uncompressedSizeBuffer.Length + compressedSizeBuffer.Length, Data.Length);
+                // Allocating resultant byte array
+                Int32 resultantBufferLength = uncompressedSizeBuffer.Length + compressedSizeBuffer.Length + Data.Length;
+                Byte[] resultantBuffer = new Byte[resultantBufferLength];
 
-            return resultantBuffer;
+                // Copying metadata and comressed data to the resultant array
+                Array.Copy(uncompressedSizeBuffer, 0, resultantBuffer, 0, uncompressedSizeBuffer.Length);
+                Array.Copy(compressedSizeBuffer, 0, resultantBuffer, uncompressedSizeBuffer.Length, compressedSizeBuffer.Length);
+                Array.Copy(Data, 0, resultantBuffer, uncompressedSizeBuffer.Length + compressedSizeBuffer.Length, Data.Length);
+
+                return resultantBuffer;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An unhandled exception has occured while attempting to convert a GZip block object to a byte array", ex);
+            }
         }
 
-        // Initialize metadata from byte buffer that represents the metadata which has been read from a compressed file
+        // Initialize compressed data buffer and metedata from byte buffer that represents the metadata (which has been read from a compressed file, for example)
         public void InitializeWithMetadata(Byte[] metadataBuffer)
         {
-            // Allocating buffers for comversion from byte array to integer value
-            Byte[] uncompressedBuffer = new Byte[MetadataUncompressedBlockSizeBufferLength];
-            Byte[] compressedBuffer = new Byte[MetadataCompressedBlockSizeBufferLength];
+            try
+            {
+                // Allocating buffers for comversion from byte array to integer value
+                Byte[] uncompressedBuffer = new Byte[MetadataUncompressedBlockSizeBufferLength];
+                Byte[] compressedBuffer = new Byte[MetadataCompressedBlockSizeBufferLength];
 
-            // Splitting metadata buffer in pieces of uncompressed and compressed block sizes
-            Array.Copy(metadataBuffer, 0, uncompressedBuffer, 0, MetadataUncompressedBlockSizeBufferLength);
-            Array.Copy(metadataBuffer, MetadataUncompressedBlockSizeBufferLength, compressedBuffer, 0, MetadataCompressedBlockSizeBufferLength);
+                // Splitting metadata buffer in pieces of uncompressed and compressed block sizes
+                Array.Copy(metadataBuffer, 0, uncompressedBuffer, 0, MetadataUncompressedBlockSizeBufferLength);
+                Array.Copy(metadataBuffer, MetadataUncompressedBlockSizeBufferLength, compressedBuffer, 0, MetadataCompressedBlockSizeBufferLength);
 
-            // Converting bytes representations of the sizes to integer
-            DataSizeUncompressed = BitConverter.ToInt32(uncompressedBuffer, 0);
-            Int32 dataSizeCompressed = BitConverter.ToInt32(compressedBuffer, 0);
+                // Converting bytes representations of the sizes to integer
+                DataSizeUncompressed = BitConverter.ToInt32(uncompressedBuffer, 0); // Write directly to the uncompressed data block size variable 
+                Int32 dataSizeCompressed = BitConverter.ToInt32(compressedBuffer, 0);
 
-            // Allocating compressed data buffer according to compressed data size
-            s_data = new Byte[dataSizeCompressed];
+                // Allocating compressed data buffer according to compressed data size
+                // Later DataSizeCompressed property will calculate compressed block size accroding to the data buffer's size
+                s_data = new Byte[dataSizeCompressed];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An unhandled exception has occured while attempting to initialize a GZip block object from a metadata buffer", ex);
+            }
         }
+        #endregion
     }
 }
